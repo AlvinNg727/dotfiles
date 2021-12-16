54020e68b32b200f6e55869d3c472c989d153c7c
AlvinNg727 <alvinng2707@gmail.com>
2021-12-16 08:47:20 +0800

    Added nerd fonts

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -11,6 +11,13 @@ sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+rm -rf paru
+
+#nerd fonts
+paru --getpkgbuild nerd-fonts-complete
+cd nerd-fonts-complete
+wget -O nerd-fonts-2.1.0.tar.gz https://github.com/ryanoasis/nerd-fonts/archive/v2.1.0.tar.gz
+makepkg -sci BUILDDIR=.
 
 #stuff i use
 sudo pacman -S fish alacritty neovim discord libreoffice-still neofetch
@@ -33,7 +40,7 @@ rm 2021-10-08.tar.gz
 cd Tela-icon-theme-2021-10-08
 ./install.sh -a
 cd ..
-rm Tela-icon-theme-2021-10-08
+rm -rf Tela-icon-theme-2021-10-08
 
 #orchis theme
 curl -OL https://github.com/vinceliuice/Orchis-theme/archive/refs/tags/2021-10-16.tar.gz
@@ -42,4 +49,4 @@ rm 2021-10-16.tar.gz
 cd Orchis-theme-2021-10-16
 ./install.sh -t grey --tweaks black
 cd ..
-rm Orchis-theme-2021-10-16
+rm -rf Orchis-theme-2021-10-16
