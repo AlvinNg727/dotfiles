f2801a7b976688461bc0ed10088e31132ac96a6c
AlvinNg727 <alvinng2707@gmail.com>
2023-01-11 13:34:31 +0800

    nvim lsp

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,7 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+    use('wbthomason/packer.nvim')
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
@@ -9,7 +9,6 @@ return require('packer').startup(function(use)
     }
 
     use('navarasu/onedark.nvim')
-
     use('nvim-tree/nvim-web-devicons')
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
@@ -27,21 +26,37 @@ return require('packer').startup(function(use)
 
     use('norcalli/nvim-colorizer.lua')
 
-    -- Lua
-    use {
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    }
     use {
         'goolord/alpha-nvim',
         config = function ()
             require'alpha'.setup(require'alpha.themes.dashboard'.config)
         end
     }
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            -- Snippet Collection (Optional)
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+
+    use('simrat39/rust-tools.nvim')
+
+    use('mfussenegger/nvim-dap')
 end)
