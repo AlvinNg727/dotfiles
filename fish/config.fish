33bbe0ee4454e79ecbb44869682a149227fe5428
AlvinNg727 <alvinng2707@gmail.com>
2023-01-13 10:02:49 +0800

    fish time

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -e fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
-
-set -U fish_greeting
-set TERM "xterm-256color"
-
 starship init fish | source
