a05ac570aae5a7945b06f316d9b85bd341e8c2d6
AlvinNg727 <alvinng2707@gmail.com>
2023-02-17 19:06:05 +0800

    nvim escape clear search

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<esc>", vim.cmd.nohlsearch)
