a6ba029276f03952f21a18cf0861c110b8f297fc
AlvinNg727 <alvinng2707@gmail.com>
2023-02-22 01:35:26 +0800

    chaned a bit post install script

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -17,9 +17,9 @@ rm -rf paru
 paru -S nerd-fonts-jetbrains-mono nerd-fonts-cascadia-code
 
 #stuff i use
-sudo pacman -S fish alacritty neovim discord libreoffice-still neofetch btop playerctl rustup steam brightnessctl light ripgrep bat
+sudo pacman -S fish alacritty neovim discord libreoffice-still neofetch btop playerctl rustup steam brightnessctl light ripgrep bat firefox
 rustup default stable
-paru -S brave-bin emote
+#paru -S brave-bin emote
 
 #swaylock
 #yay -S swaylock-effects-git
@@ -34,9 +34,12 @@ paru -S brave-bin emote
 sudo pacman -S starship
 #sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
+#anime shit
+# ani-cli trackma-git
+
 #install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #tela icon theme
 curl -OL https://github.com/vinceliuice/Tela-icon-theme/archive/refs/tags/2021-10-08.tar.gz
