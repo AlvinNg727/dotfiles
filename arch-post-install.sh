41f4ee5204b8c1bb2028a99a8211bc8ea23fb04a
AlvinNg727 <alvinng2707@gmail.com>
2023-03-14 14:16:52 +0800

    revamped post install script

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
 #Update packages
-sudo pacman -Syu
+pacman -Syu
 
 #git
-sudo pacman -S git
-
-#paru
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-rm -rf paru
+pacman -S git
 
 #nerd fonts
-paru -S nerd-fonts-jetbrains-mono nerd-fonts-cascadia-code
+pacman -S ttf-jetbrains-mono-nerd otf-cascadia-code-nerd
 
 #stuff i use
-sudo pacman -S fish alacritty neovim discord libreoffice-still neofetch btop playerctl rustup steam brightnessctl light ripgrep bat firefox
+pacman -S fish alacritty neovim discord neofetch btop playerctl rustup steam brightnessctl light ripgrep bat firefox
 rustup default stable
-yay -S nwg-look-bin ani-cli-git dell-bios-fan-control-git trackma-git
-#paru -S brave-bin emote
+$1 -S nwg-look-bin ani-cli-git dell-bios-fan-control-git trackma-git brave-bin emote
 
 #dunst
 #yay -S dunst
@@ -38,33 +30,9 @@ yay -S nwg-look-bin ani-cli-git dell-bios-fan-control-git trackma-git
 #sudo pacman -S ttf-cascadia-code noto-fonts noto-fonts-emoji
 
 #starship
-sudo pacman -S starship
+pacman -S starship
 #sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-
-#install vim-plug
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #theme shit
 sudo pacman -S materia-gtk-theme
-yay -S papirus-folders-git
-yay -S papirus-icon-theme
-yay -s xcursor-simp1e-dark
-
-#tela icon theme
-#curl -OL https://github.com/vinceliuice/Tela-icon-theme/archive/refs/tags/2021-10-08.tar.gz
-#tar -xzvf 2021-10-08.tar.gz
-#rm 2021-10-08.tar.gz
-#cd Tela-icon-theme-2021-10-08
-#./install.sh -a
-#cd ..
-#rm -rf Tela-icon-theme-2021-10-08
-
-#orchis theme
-#curl -OL https://github.com/vinceliuice/Orchis-theme/archive/refs/tags/2021-10-16.tar.gz
-#tar -xzvf 2021-10-16.tar.gz
-#rm 2021-10-16.tar.gz
-#cd Orchis-theme-2021-10-16
-#./install.sh -t grey --tweaks black
-#cd ..
-#rm -rf Orchis-theme-2021-10-16
+$1 -S papirus-folders-git papirus-icon-theme xcursor-simp1e-dark
