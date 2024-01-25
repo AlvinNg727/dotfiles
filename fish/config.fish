30b7d7c6f3d091d6c2bba078d0d199988e71722f
Alvin <alvinng2707@gmail.com>
2024-01-25 09:14:11 +0800

    Added git recursive aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+alias gcr="git clone --recurse-submodules -j8"
+alias gpr="git pull --recurse-submodules -j8"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
