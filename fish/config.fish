669ca396c7716d7c19432e68ca44e45e71a95b0a
Alvin Ng <alvinng2707@gmail.com>
2024-05-24 18:12:25 +0800

    rip ls, hi eza :)

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 alias gcr="git clone --recurse-submodules -j8"
 alias gpr="git pull --recurse-submodules -j8"
+alias ls="eza -x --color=always --icons=always --group-directories-first --git --git-repos"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
