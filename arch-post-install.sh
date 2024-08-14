236a7717cd35077acab70353a1c4e52fde645421
AlvinNg727 <alvinng2707@gmail.com>
2024-08-14 23:43:37 +0800

    update formatting

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#Update packages
+# Update packages
 pacman -Syu
 
 # git
 pacman -S git github-cli
 
-#nerd fonts
+# nerd fonts
 pacman -S ttf-cascadia-code-nerd
 
-#stuff i use
+# stuff i use
 pacman -S fish alacritty neovim discord neofetch btop playerctl openssh rustup steam brightnessctl light bat firefox wl-clipboard xdg-user-dirs thunar file-roller thunar-archive-plugin tumbler eza bc
 rustup default stable
 $1 -S ani-cli-git trackma-git brave-bin emote
@@ -23,18 +23,18 @@ $1 -S ani-cli-git trackma-git brave-bin emote
 # waybar
 # $1 -S waybar
 
-#dunst
+# dunst
 # $1 -S dunst libnotify
 
-#rofi
+# rofi
 # $1 -S rofi-wayland
 
-#fonts
+# fonts
 pacman -S noto-fonts noto-fonts-emoji noto-fonts-cjk
 
-#starship
+# starship
 pacman -S starship
 
-#theme shit
+# theme shit
 sudo pacman -S materia-gtk-theme
 $1 -S papirus-icon-theme-git xcursor-simp1e-dark
