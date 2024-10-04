10c8443b4230f9051fa648cfda38527969b8d3f7
AlvinNg727 <alvinng2707@gmail.com>
2024-10-05 02:36:41 +0800

    add yazi config

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,21 @@ alias gcr="git clone --recurse-submodules -j8"
 alias gpr="git pull --recurse-submodules -j8"
 alias ls="eza -x --color=always --icons=always --group-directories-first --git --git-repos"
 
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 pyenv init - | source
 
-fzf --fish | source
+# fzf --fish | source
 
 starship init fish | source
