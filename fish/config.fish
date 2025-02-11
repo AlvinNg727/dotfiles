5e697d214733a75868b1b545cbd6e43f7a6d3cb0
AlvinNg727 <alvinng2707@gmail.com>
2025-02-11 20:10:10 +0800

    add lazygit alias

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,13 @@ alias gcr="git clone --recurse-submodules -j8"
 alias gpr="git pull --recurse-submodules -j8"
 alias ls="eza -x --color=always --icons=always --group-directories-first --git --git-repos"
 
+alias gg="lazygit"
+
+# Pokemon :D
+# function fish_greeting
+#     pokemon-colorscripts -rn charmander,charizard,gengar,greninja,rayquaza --no-title
+# end
+
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
@@ -15,9 +22,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-pyenv init - | source
+set fzf_preview_dir_cmd eza --all --color=always
+set fzf_preview_file_cmd bat
+set fzf_diff_highlighter delta --color-only --paging=never
 
-# fzf --fish | source
+pyenv init - | source
 
 zoxide init fish --cmd cd | source
 
