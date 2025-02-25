915e70953ed4a68270a5572376698a86a2e73c8e
AlvinNg727 <alvinng2707@gmail.com>
2025-02-25 23:43:23 +0800

    add: jsonc to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
                 "dockerfile",
                 "fish",
                 "json",
+                "jsonc",
                 "lua",
                 "markdown",
                 "markdown_inline",
