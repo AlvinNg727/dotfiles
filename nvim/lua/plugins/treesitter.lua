cb6beae4192ad8a5f639dea15bf2aa77b0f6cee7
AlvinNg727 <alvinng2707@gmail.com>
2025-02-26 16:34:51 +0800

    decrease treesitter context max lines

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -50,6 +50,8 @@ return {
         "nvim-treesitter/nvim-treesitter-context",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         event = "VeryLazy",
-        opts = {},
+        opts = {
+            max_lines = 3,
+        },
     },
 }
