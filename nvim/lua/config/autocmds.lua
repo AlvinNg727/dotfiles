0287721333e13cda10210dd3180823036f0945d0
AlvinNg727 <alvinng2707@gmail.com>
2025-09-01 14:41:05 +0800

    feat: nvim autocmd to restore cursor

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         (vim.hl or vim.highlight).on_yank()
     end,
 })
+
+-- Set guicursor for VimEnter and VimResume
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+    callback = function()
+        vim.opt.guicursor =
+            "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+    end,
+})
+
+-- Set guicursor for VimLeave and VimSuspend
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+    callback = function()
+        vim.opt.guicursor = "a:hor20-blinkwait700-blinkoff500-blinkon250"
+    end,
+})
